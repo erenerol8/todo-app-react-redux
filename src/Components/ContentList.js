cc885788bfc50c6f2e25ba616bbc0d6b3945aae7
Eren <erenerol8@gmail.com>
2022-11-30 19:55:35 +0300

    commit

--- src/Components/ContentList.js
+++ src/Components/ContentList.js
@@ -1,24 +1,41 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggle, destroy, getTodosAsync } from "../redux/todos/todosSlice";
 
 const ContentList = () => {
   const items = useSelector((state) => state.todos.items);
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.todos.İsloading);
+  const error = useSelector((state) => state.todos.error);
+
+  useEffect(() => {
+    dispatch(getTodosAsync());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Something is off</div>;
+  }
 
   return (
     <ul className="todo-list">
-      {/* <li className="completed">
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn JavaScript</label>
-          <button className="destroy"></button>
-        </div>
-      </li> */}
       {items.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
-            <input className="toggle" type="checkbox" />
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => dispatch(destroy(item.id))}
+            ></button>
           </div>
         </li>
       ))}
--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -1,27 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const getTodosAsync = createAsyncThunk(
+  "todos/getTodosAsync",
+  async () => {
+    const res = await fetch("http://localhost:7000/todos");
+    return await res.json();
+  }
+);
 
 export const todosSlice = createSlice({
   name: "todos",
   initialState: {
-    items: [
-      {
-        id: "1",
-        title: "Learn react",
-        completed: false,
-      },
-      {
-        id: "2",
-        title: "hello world",
-        completed: true,
-      },
-    ],
+    items: [],
+    isLoading: false,
+    error: null,
   },
   reducers: {
-    addTodo: (state, actions) => {
-      state.items.push(actions.payload);
+    addTodo: (state, action) => {
+      state.items.push(action.payload);
+    },
+    toggle: (state, action) => {
+      const { id } = action.payload;
+      const item = state.items.find((item) => item.id === id);
+      item.completed = !item.completed;
+    },
+    destroy: (state, action) => {
+      const id = action.payload;
+
+      const filtered = state.items.filter((item) => item.id !== id);
+      state.items = filtered;
+    },
+  },
+  extraReducers: {
+    [getTodosAsync.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [getTodosAsync.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.isLoading = false;
+    },
+    [getTodosAsync.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
     },
   },
 });
 
-export const { addTodo } = todosSlice.actions;
+export const { addTodo, toggle, destroy } = todosSlice.actions;
 export default todosSlice.reducer;
